65645433a30242611e253fa634a33804a17ebe15
Joao Mateus <jmateussc2706@gmail.com>
2025-04-05 02:49:47 -0300

    ex012

---
+++ capitulo05/ex012.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int cont, num, i;
+    int Tprimos = 0, result, divisores;
+
+    for (cont = 0; cont < 10; cont++)
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &num);
+
+        divisores = 0;
+        for (i = 1; i <= num; i++)
+        {
+            result = (num % i);
+            if (result == 0)
+            {
+                divisores++;
+            }
+        }
+
+        if (divisores == 2)
+        {
+            Tprimos++;
+        }
+    }
+
+    printf("Total de numeros primos digitados: %d", Tprimos);
+     
+    return 0;
+}
