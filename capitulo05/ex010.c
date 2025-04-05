5e6d32ee021af5ed8abe68260f4d168449179e33
Joao Mateus <jmateussc2706@gmail.com>
2025-04-05 00:00:12 -0300

    ex010

---
+++ capitulo05/ex010.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, somai, somap;
+    int v[10];
+
+    somap = 0;
+    somai = 0;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Digite o %do. valor: ", i+1);
+        scanf("%d", &v[i]);
+
+        if (v[i] % 2 == 0)
+        {
+            somap += v[i];
+        }
+        else
+        {
+            somai += v[i];
+        } 
+        
+    }
+
+    printf("\nSoma dos numeros pares = %d\n", somap);
+    printf("Soma dos numeros impares = %d\n", somai);
+
+    return 0;
+}
